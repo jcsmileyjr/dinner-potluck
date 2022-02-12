bc91088b2751d383e9f2f222e68552d1869c5ae1
Smiley <jcsmileyjr@gmail.com>
2022-02-11 20:40:43 -0600

    Updates to pillbox component for event page

--- dinner/src/components/Pillbox/Pillbox.js
+++ dinner/src/components/Pillbox/Pillbox.js
@@ -6,15 +6,16 @@ const Pillbox = ({imageType, headerTitle, children, buttonColor, buttonTitle, sh
     return(
         <div className="pillbox--container">
             <div className="pillbox__header--style">
-                <h2>{headerTitle}</h2>
+                <h2 className="">{headerTitle}</h2>
                 <img src={imageType==="heart"?heart:chef} className="img--style" alt="" />
             </div>
+            <input type="text" className="pillbox__input--style" />
             {showButton==="true" &&
                 <button type="button" className={`pillbox__button--style ${buttonColor==='light'?'lightGreen':'darkGreen'}`}>
                     {buttonTitle}
                 </button>
             }
-           <p className="pillbox__text--style">{children}</p>
+           <div className="pillbox__text--style">{children}</div>
            
         </div>
     );
