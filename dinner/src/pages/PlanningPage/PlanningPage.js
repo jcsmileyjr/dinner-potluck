1d1dd79e12b6296dcb028cf1032871afa45e8784
Smiley <jcsmileyjr@gmail.com>
2022-02-13 13:52:49 -0600

    Refactor mobile and desktop layout responsivness

--- dinner/src/pages/PlanningPage/PlanningPage.js
+++ dinner/src/pages/PlanningPage/PlanningPage.js
@@ -3,14 +3,24 @@ import Header from "../../components/Header/header";
 import Pillbox from "../../components/Pillbox/Pillbox";
 import LineItem from "../../components/LineItem/LineItem";
 import Menu from "../../components/Menu/Menu";
+import React, {useState} from 'react';
 
 const PlanningPage = ({goto, event}) => {
-    console.log(event);
+    /**
+     * Add event data to component state via useEffect
+     * Create function that calls new component UpdateMenu
+     * Component UpdateMenu takes in the "food item" or allow write in, user input "name"
+     * It has a function that update the planning page's menu
+     * 
+     */
+
+    const [eventData, setEventData] = useState(event);
+
     return(
         <div>
             <main className="planningPage--container">
                 <Header event={goto} headerButtonTitle="Home" />
-                <h1 className="planning__pageTitle--style">{event.EventTitle}</h1>
+                <h1 className="planning__pageTitle--style">{eventData.EventTitle}</h1>
                 <div className="planningPage__main--container">
                     <section className="planningPage__menu--container">
                         <Menu 
@@ -19,16 +29,16 @@ const PlanningPage = ({goto, event}) => {
                             buttonTitle="Write in your meal"
                             showButton= "true"                            
                         >
-                            {event.menu.map((item, index) => {
+                            {eventData.menu.map((item, index) => {
                                 return(
-                                    <>
+                                    <div key={index}>
                                         {item.asignee === 'none' &&
                                             <LineItem leftContent={item.food} hasButton="true" buttonTitle="I want this" />
                                         }
                                         {item.asignee !== 'none' &&
                                             <LineItem leftContent={item.food} rightContent={item.asignee} />
                                         }
-                                    </>
+                                    </div>
                                 );
                             })
                             }
