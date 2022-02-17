e0ae838f871e5152b61577ceca9228a07af40db0
Smiley <jcsmileyjr@gmail.com>
2022-02-17 17:04:34 -0600

    Feature: Create Page part 2 showing the new menu and event
    Added a layout for the phase 2 part of the create page that include menu

--- dinner/src/pages/CreatePage/CreatePage.js
+++ dinner/src/pages/CreatePage/CreatePage.js
@@ -2,17 +2,23 @@ import "./createPage.css";
 import React, { useState } from "react";
 import Header from "../../components/Header/header";
 import EventWizard from "../../components/EventWizard/EventWizard";
+import Menu from "../../components/Menu/Menu";
+import Pillbox from "../../components/Pillbox/Pillbox";
+import LineItem from "../../components/LineItem/LineItem";
 
 const CreatePage = ({ goto, gotoEventPage, createEvent}) => {
   const [showModal, setShowModal] = useState(true);
   const [currentWizardData, setCurrentWizardData] = useState("");
   const [currentWizard, setCurrentWizard] = useState(1);
   const [newEvent, setNewEvent] = useState({menu:[]});
+  const [pageTitle, setPageTitle] = useState("Create an Event");
+  const [pageDate, setPageDate] = useState("None");
 
   const confirmWizard = () => {
     console.log(currentWizardData);
     let wizardEvent = newEvent;
     if(currentWizard === 1){
+        setPageTitle(currentWizardData)
         wizardEvent.EventTitle = currentWizardData;
         setNewEvent(newEvent => ({
             ...newEvent, ...wizardEvent
@@ -20,6 +26,7 @@ const CreatePage = ({ goto, gotoEventPage, createEvent}) => {
         setCurrentWizard(prevCurrent => prevCurrent + 1);
     }
     if(currentWizard === 2){
+        setPageDate(currentWizardData)
         wizardEvent.EventDate = currentWizardData;
         setNewEvent(newEvent => ({
             ...newEvent, ...wizardEvent
@@ -58,14 +65,34 @@ const CreatePage = ({ goto, gotoEventPage, createEvent}) => {
     }
 
     setShowModal(false);
-    createEvent(newEvent);
+    //createEvent(newEvent);
+    setCurrentWizard("final");
   }
+
+  const openUpdateMenu = (foodItem) => {
+    setShowModal(true);
+    //setChosenFood(foodItem);
+    //setUserInputtedName("");
+  };
+
+  const closeUpdateMenu = () => {
+    setShowModal(false);
+    //setUserInputtedName("");
+  };
+
+  const confirmUpdateMenu = () => {
+    //confirmPicked(chosenFood, userInputtedName);
+    closeUpdateMenu();
+  };  
 
   return (
     <div>
       <main className="createPage--container">
         <Header event={goto} headerButtonTitle="Home" />
-        <h1 className="createPage__pageTitle--style">Create an Event</h1>
+        <div className="createPage__titleArea--container">
+            <h1 className="createPage__pageTitle--style">{pageTitle}</h1>
+            <span className="createPage__pageDate--style">{pageDate}</span>
+        </div>
         <div className="createPage__main--container">
             {currentWizard === 1 && 
                 <EventWizard
@@ -97,8 +124,57 @@ const CreatePage = ({ goto, gotoEventPage, createEvent}) => {
                     closeWizardData={finishedWizard}
                     isDone = {true}
                 />
-            }            
-        </div>
+            } 
+            {currentWizard === "final" &&
+                <div className="createPage__main--container">
+                    <section className="planningPage__menu--container">
+                        <Menu headerTitle="Menu" >
+                        {newEvent.menu.map((item, index) => {
+                            return (
+                            <div key={index}>
+                                {item.asignee === "none" && (
+                                <LineItem
+                                    leftContent={item.food}
+                                    hasButton="true"
+                                    buttonTitle="Designate"
+                                    buttonEvent={() => openUpdateMenu(item.food)}
+                                />
+                                )}
+                                {item.asignee !== "none" && (
+                                <LineItem
+                                    leftContent={item.food}
+                                    rightContent={item.asignee}
+                                />
+                                )}
+                            </div>
+                            );
+                        })}
+                        </Menu>
+                    </section>
+                    <section className="planningPage__popularItems--container">
+                        <Pillbox
+                        headerTitle="Pick a Popular Item"
+                        buttonTitle="Write in your meal"
+                        showButton="true"
+                        >
+                            View dozens of popular food items with pictures, images, and
+                            recipes.
+                        </Pillbox>
+                        {/* <UpdateMenu
+                            isVisible={showModal}
+                            closeUpdateMenu={closeUpdateMenu}
+                            prepickedFood={chosenFood}
+                            inputName = {userInputtedName}
+                            inputFoodEvent={setChosenFood}
+                            inputNameEvent={setUserInputtedName}
+                            confirmEvent={() => {
+                                confirmUpdateMenu();
+                            }}
+                        />                         */}
+                    </section>
+                </div>
+            } 
+            </div>          
       </main>
     </div>
   );
