6caad5d22c5079c15e8c4435cdcb278ad32b6ec4
Smiley <jcsmileyjr@gmail.com>
2022-02-25 07:58:12 -0600

    Refactor Create wizards to add a date picker

--- dinner/src/components/EventWizard/EventWizard.js
+++ dinner/src/components/EventWizard/EventWizard.js
@@ -5,7 +5,12 @@ const EventWizard = ({isVisible, eventWizardTitle, eventWizardData, getWizardDat
     return(
         <section className={`eventWizard--container ${isVisible?'':'hide'}`}>   
             <h2 className="eventWizard__title--style">{eventWizardTitle}</h2>
-            <input type="text" value={eventWizardData} className="pillbox__input--style" onChange={(e) => {getWizardData(e.target.value)}} />
+            {eventWizardTitle !== "What is the Event's Date" &&
+                <input type="text" value={eventWizardData} className="pillbox__input--style" onChange={(e) => {getWizardData(e.target.value)}} />
+            }
+            {eventWizardTitle === "What is the Event's Date" &&
+                <input type="date" value={eventWizardData} className="pillbox__input--style" onChange={(e) => {getWizardData(e.target.value)}} />
+            }
             <button type="button" onClick={()=> {confirmWizrdData()}} className="updateMenu__button--style confirmButton--style">
                     Confirm
             </button>
